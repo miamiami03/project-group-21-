a50e3a9b5a0adb1b986b3d624a07a471a9cd7ac4
miamiami03 <miadequito03@gmail.com>
2025-05-17 19:18:47 +0800

    Add files via upload

---
+++ src/project_rheasalon/Project_RheaSalon.java
@@ -0,0 +1,17 @@
+
+package project_rheasalon;
+
+public class Project_RheaSalon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmSignIn Login = new frmSignIn();
+        Login.setVisible(true);
+        Login.pack();
+        Login.setLocationRelativeTo(null);
+    }
+    
+}
